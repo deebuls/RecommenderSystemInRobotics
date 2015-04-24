a9382f4a15e77273c94973a7ec21edce35d447da
deebuls <deebuls@gmail.com>
2015-04-24 16:23:35 +0200

    Automation added data accumulator script

---
+++ experiments/dataAccumulator.py
@@ -0,0 +1,71 @@
+#!/home/deebuls/anaconda/bin/python
+
+import sys, getopt
+import os
+import glob
+import json
+
+def main(argv):
+    inputfile = ''
+    outputfile = ''
+
+    try:
+        opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+    except getopt.GetoptError:
+        print 'test.py -i <inputfolder> -o <outputfile>'
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print 'test.py -i <inputfile> -o <outputfile>'
+            print 'inputfolder : The folder in which all the json files are stored'
+            print '              default : /tmp'
+            print 'outputfile : The name of the experiments. A folder will be created '
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            inputfile = arg
+        elif opt in ("-o", "--ofile"):
+            outputfile = arg
+
+    if (outputfile == ''):
+        print 'ERROR : Please specify experiment name'
+        sys.exit()
+    if (inputfile == ''):
+        inputfile = '/tmp/'
+    print 'Input file is "', inputfile
+    print 'Output file is "', outputfile
+
+    return inputfile, outputfile
+
+if __name__ == "__main__":
+    inputfile, outputfile = main(sys.argv[1:])
+
+    print 'Creating Directory ', outputfile
+    os.system("mkdir " + outputfile)
+    print 'Copying all json files from ', inputfile, " to ", outputfile
+    os.system("mv " + inputfile + "*.json" + " " + outputfile)
+    read_files = sorted(glob.glob("./reach/i*.json"))
+    output_list = []
+
+    print 'reading all the initial files'
+    for f in read_files:
+        with open(f, "rb") as infile:
+            output_list.append(json.load(infile))
+
+    print 'creating the combined json INITIAL file'
+    with open("./" + outputfile + "/" + "initial.json", "wb") as outfile:
+        json.dump(output_list, outfile)
+
+    del read_files
+    print 'reading all the FINAL files'
+    read_files = sorted(glob.glob("./reach/f*.json"))
+    output_list = []
+
+    for f in read_files:
+        with open(f, "rb") as infile:
+            output_list.append(json.load(infile))
+
+    print 'creating the combined json FINAL file'
+    with open("./" + outputfile + "/" + "final.json", "wb") as outfile:
+        json.dump(output_list, outfile)
+
+
